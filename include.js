b26cc45695961b0680bce300f2fea33db9bf54be
mfrancis95 <mikefrancis95@gmail.com>
2016-03-05 18:21:50 -0500

    Add data-method attribute

--- include.js
+++ include.js
@@ -1,12 +1,22 @@
 var elements = document.getElementsByClassName("include");
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
+    var dataset = element.dataset;
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            element.innerHTML += request.responseText;
+            switch (dataset.method) {
+                case "append":
+                    element.innerHTML += request.responseText;
+                    break;
+                case "prepend":
+                    element.innerHTML = request.responseText + element.innerHTML;
+                    break;
+                default:
+                    element.innerHTML = request.responseText;
+            }
         }
     };
-    request.open("GET", element.dataset.file);
+    request.open("GET", dataset.file);
     request.send();
 }
