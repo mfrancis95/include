dffacc2a93bd095d4a67d7ae4870175800112681
mfrancis95 <mikefrancis95@gmail.com>
2016-03-08 15:20:04 -0500

    Move AJAX code into its own function

--- include.js
+++ include.js
@@ -1,22 +1,27 @@
+Element.prototype.include = function(file, method) {
+    var self = this;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            switch (method) {
+                case "append":
+                    self.innerHTML += request.responseText;
+                    break;
+                case "prepend":
+                    self.innerHTML = request.responseText + self.innerHTML;
+                    break;
+                default:
+                    self.innerHTML = request.responseText;
+            }
+        }
+    };
+    request.open("GET", file);
+    request.send();
+};
+
 var elements = document.getElementsByClassName("include");
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     var dataset = element.dataset;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            switch (dataset.method) {
-                case "append":
-                    element.innerHTML += request.responseText;
-                    break;
-                case "prepend":
-                    element.innerHTML = request.responseText + element.innerHTML;
-                    break;
-                default:
-                    element.innerHTML = request.responseText;
-            }
-        }
-    };
-    request.open("GET", dataset.file);
-    request.send();
+    element.include(dataset.file, dataset.method);
 }
