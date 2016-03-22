d3a1489139c187c7a3602f6141231f6339b7038b
mfrancis95 <mikefrancis95@gmail.com>
2016-03-22 12:11:15 -0400

    Add all-statuses attribute

--- include.js
+++ include.js
@@ -1,8 +1,8 @@
-Element.prototype.include = function(file, method) {
+Element.prototype.include = function(file, method, allStatuses) {
     var self = this;
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        if (request.readyState === XMLHttpRequest.DONE && (allStatuses || request.status === 200)) {
             switch (method) {
                 case "append":
                     self.innerHTML += request.responseText;
@@ -23,5 +23,5 @@ var elements = document.getElementsByClassName("include");
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     var dataset = element.dataset;
-    element.include(dataset.file, dataset.method);
+    element.include(dataset.file, dataset.method, dataset.allStatuses);
 }
